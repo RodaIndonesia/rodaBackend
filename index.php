af2b21f0c7fe3aaf2015cf29e6f44564ba34892a
RodaIndonesia <58638883+RodaIndonesia@users.noreply.github.com>
2019-12-09 09:49:41 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 $server_key = "SB-Mid-server-32zWD6Vmw5IlIAKq9JJtNKVD";
 
 $is_production = false;
-$api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' : 'https://app.sandbox.midtrans.com/snap/v1/transactions';
+$api_url = $is_production ? 
+	'https://app.midtrans.com/snap/v1/transactions' : 
+	'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 if (!strpos($_SERVER['REQUEST_URI'], '/charge')) {
 	http_response_code(404);
@@ -34,7 +36,7 @@ function chargeAPI($api_url, $server_key, $request_body){
 		CURLOPT_HTTPHEADER => array(
 			'Content-Type: application/json',
 			'Accept: application/json',
-			'Authorization: Basic ' : base64_encode($server_key . ':')
+			'Authorization: Basic ' . base64_encode($server_key . ':')
 		),
 		CURLOPT_POSTFIELDS => $request_body
 	);
